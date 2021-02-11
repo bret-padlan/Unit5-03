9ccd18ec7a1251e3ae8d7edd6a71be3f6ed17d0c
bretpadlan <bret.padlan@ocsbstudent.ca>
2021-02-11 17:13:07 +0000

    add eventlistener to button & button2

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-function button() {
-  document.getElementById("paragraph2").innerHTML = "This should work, right?";
-  document.body.style.backgroundColor = "chocolate"
+document.getElementById('button').addEventListener('click', button)
+function button () {
+  document.getElementById('paragraph2').innerHTML = 'This should work, right?'
+  document.body.style.backgroundColor = 'chocolate'
 }
 
-function button2() {
-  document.getElementById("paragraph2").innerHTML = "Yes, it should";
-  document.body.style.backgroundColor = "lightblue" 
+document.getElementById('button2').addEventListener('click', button2)
+function button2 () {
+  document.getElementById('paragraph2').innerHTML = 'Yes, it should'
+  document.body.style.backgroundColor = 'lightblue'
 }
 
-document.getElementById("add").addEventListener("click", diskCord)
-function diskCord() {
-  alert ("ERROR DO NOT CLICK OK")
-  document.getElementById("back").style.backgroundColor = "red"
+document.getElementById('add').addEventListener('click', diskCord)
+function diskCord () {
+  alert('ERROR DO NOT CLICK OK')
+  document.getElementById('back').style.backgroundColor = 'red'
 }
+
+//change text & ackground colours through buttons & add warning button.
